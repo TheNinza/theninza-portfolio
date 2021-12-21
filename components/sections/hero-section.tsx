3df85efc440262c70ac6bdd1cf2ccb5597e7d448
TheNinza <niks.a3198@gmail.com>
2021-12-21 12:04:27 +0530

    fixed responsive issue with the hero text

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 // styled-components
 
 const HeroSectionContainer = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   width: 100%;
 
   /* align items to center of the container */
@@ -26,14 +26,16 @@ const BigHeading = styled.h1`
   font-size: 7vw;
   font-weight: ${({ theme }) => theme.fontWeights.bold};
 
-  @media only screen and (max-width: 1050px) {
-    font-size: 5rem;
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.xl}px) {
+    font-size: 6.9vw;
   }
-  @media only screen and (max-width: 640px) {
-    font-size: 4rem;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.md}px) {
+    font-size: 10vw;
   }
-  @media only screen and (max-width: 510px) {
-    font-size: 3rem;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}px) {
+    font-size: 12vw;
   }
 `;
 
@@ -41,6 +43,10 @@ const OutlinedBigHeading = styled(BigHeading)`
   color: transparent;
   -webkit-text-stroke: 2px ${({ theme }) => theme.colors.textPrimary};
   margin-top: 5rem;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}px) {
+    margin-top: 3rem;
+  }
 `;
 
 const HeroSection = () => {
