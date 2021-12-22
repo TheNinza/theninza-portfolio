da1be011a2ba0bee3f5ab6d9a3b800815757f00c
TheNinza <niks.a3198@gmail.com>
2021-12-22 13:38:16 +0530

    added touch based events to control cursor and better ref management

--- components/custom-cursor.tsx
+++ components/custom-cursor.tsx
@@ -18,15 +18,32 @@ const CustomCursorContainer = styled.div`
 
   transition: all 0.3s ease;
   transition-property: background-color, transform;
+
+  display: none;
 `;
 
 const CustomCursor: FC = () => {
   const cursorRef = useCallback((cursor: HTMLDivElement) => {
     if (cursor == null) return;
+
     window.addEventListener("mousemove", (e) => {
       if (cursor) {
-        cursor.style.left = `${e.clientX}px`;
-        cursor.style.top = `${e.clientY}px`;
+        // if cursor is not within 5px of the edge of the screen, then don't show it
+        if (
+          e.clientX < 10 ||
+          e.clientX > window.innerWidth - 10 ||
+          e.clientY < 10 ||
+          e.clientY > window.innerHeight - 10
+        ) {
+          cursor.style.display = "none";
+        }
+
+        // if cursor is in the viewport, show it
+        else {
+          cursor.style.display = "block";
+          cursor.style.left = `${e.clientX}px`;
+          cursor.style.top = `${e.clientY}px`;
+        }
       }
     });
 
@@ -41,6 +58,25 @@ const CustomCursor: FC = () => {
         cursor.style.background = "#fff";
       }
     });
+
+    window.addEventListener("touchstart", (e) => {
+      if (cursor) {
+        cursor.style.display = "block";
+      }
+    });
+
+    window.addEventListener("touchend", (e) => {
+      if (cursor) {
+        cursor.style.display = "none";
+      }
+    });
+
+    window.addEventListener("touchmove", (e) => {
+      if (cursor) {
+        cursor.style.left = `${e.touches[0].clientX}px`;
+        cursor.style.top = `${e.touches[0].clientY}px`;
+      }
+    });
   }, []);
 
   useIsomorphicLayoutEffect(() => {
@@ -48,6 +84,9 @@ const CustomCursor: FC = () => {
       window.removeEventListener("mousemove", () => {});
       window.removeEventListener("mousedown", () => {});
       window.removeEventListener("mouseup", () => {});
+      window.removeEventListener("touchmove", () => {});
+      window.removeEventListener("touchstart", () => {});
+      window.removeEventListener("touchend", () => {});
     };
   }, []);
 
