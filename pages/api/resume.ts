fb7ee3320e0ad3cbf61764483ec44b96278efd1b
TheNinza <niks.a3198@gmail.com>
2022-01-31 13:47:14 +0530

    added backend graphql auth and piped the res to route to avoid url redirect to graphcms url

--- pages/api/resume.ts
+++ pages/api/resume.ts
@@ -1,11 +1,13 @@
 import { GraphQLClient, gql } from "graphql-request";
 import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 interface IGetResume {
   (req: NextApiRequest, res: NextApiResponse): Promise<void>;
 }
 
 const NEXT_PUBLIC_GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+const GRAPH_AUTH_TOKEN = process.env.GRAPH_AUTH_TOKEN;
 
 const getresume: IGetResume = async (req, res) => {
   if (!NEXT_PUBLIC_GRAPHQL_ENDPOINT) {
@@ -15,7 +17,11 @@ const getresume: IGetResume = async (req, res) => {
     return;
   }
 
-  const client = new GraphQLClient(NEXT_PUBLIC_GRAPHQL_ENDPOINT);
+  const client = new GraphQLClient(NEXT_PUBLIC_GRAPHQL_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${GRAPH_AUTH_TOKEN}`,
+    },
+  });
 
   const getResumeQuery = gql`
     query GetResume {
@@ -32,9 +38,17 @@ const getresume: IGetResume = async (req, res) => {
 
   const data = await client.request(getResumeQuery);
 
-  const url = data.resumes[0].resumeFile.url;
+  const pdfUrl = data.resumes[0].resumeFile.url;
 
-  res.redirect(url);
+  const urlContent = await axios.get(pdfUrl, {
+    responseType: "stream",
+  });
+
+  res.setHeader("Content-Type", "application/pdf");
+
+  urlContent.data.pipe(res);
+
+  return;
 };
 
 export default getresume;
