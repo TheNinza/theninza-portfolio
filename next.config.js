67a7601192d846a4ff63aff1b0b623775d5c71f5
TheNinza <niks.a3198@gmail.com>
2022-02-23 12:01:40 +0530

    try 3

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["media.graphcms.com", "i.scdn.co"],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
